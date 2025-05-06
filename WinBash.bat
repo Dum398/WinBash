fc02dbeeb66f30c99a3f93ab336419144950f2ba
jirka <jirikmachula@gmail.com>
2025-05-06 13:09:53 +0200

    new commands

--- WinBash.bat
+++ WinBash.bat
@@ -1,7 +1,7 @@
 @echo off
 rem winbash console
 :start
-set /p command="enter command"
+set /p command="enter command: "
 if %command%==ls (
 dir
 goto start)else if %command%==pwd (
@@ -16,5 +16,15 @@ goto start)else if %command%==exit (
 goto eof
 )else if %command%==nproc (
 tasklist
+goto start)else if %command%==df (
+chkdsk C:
+goto start)else if %command%==mkdir (
+set /p dirname="set the name for this directory: "
+mkdir %dirname%
+goto start)else if %command%==date (
+date /T
+goto start)else if %command%==apt_install (
+set /p pkg="what do you want to install: "
+winget install "%pkg%"
 goto start)
 pause
