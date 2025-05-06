1af09f612859c47b9fe7c0edbccfc773315cc051
jirka <jirikmachula@gmail.com>
2025-05-06 13:26:48 +0200

    upravy a opravy

--- WinBash.bat
+++ WinBash.bat
@@ -26,5 +26,34 @@ date /T
 goto start)else if %command%==apt_install (
 set /p pkg="what do you want to install: "
 winget install "%pkg%"
+goto start)else if %command%==apt_remove (
+set /p removepkg="what do you want to remove: "
+winget uninstall "%removepkg%"
+goto start)else if %command%==apt_update (
+winget upgrade
+goto start)else if %command%==apt_search (
+set /p searchpkg="what do you want to search: "
+winget search "%searchpkg%"
+goto start)else if %command%==apt_list (
+winget list
+goto start)else if %command%==apt_show (
+set /p showpkg="what do you want to show: "
+winget show "%showpkg%"
+goto start)else if %command%==apt_upgrade (
+winget upgrade --all
+goto start)else if %command%==mv (
+set /p mvfile="enter the file you want to move: "
+set /p mvdir="enter the directory you want to move to: "
+move %mvfile% %mvdir%
+echo %mvfile% moved to %mvdir%
+goto start)else if %command%==cp (
+set /p cpfile="enter the file you want to copy: "
+set /p cpdir="enter the directory you want to copy to: "
+copy %cpfile% %cpdir%
+echo %cpfile% copied to %cpdir%
+goto start)else if %command%==rm (
+set /p rmfile="enter the file you want to remove: "
+del "%rmfile%"
+echo %rmfile% removed
 goto start)
 pause
